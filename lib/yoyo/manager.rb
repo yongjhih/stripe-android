f24f0eea4278c173b0f9bf7f1157b25f2eee85d9
Andreas Fuchs <asf@boinkor.net>
2014-06-13 17:57:35 -0700

    Don't not ssh agent forward

--- lib/yoyo/manager.rb
+++ lib/yoyo/manager.rb
@@ -67,8 +67,7 @@ module Yoyo
       log.debug "Starting SSH connection for #{user}"
       begin
         SpaceCommander::SSH::Connection.new(user, ip_address,
-                                            opts.merge(:user_known_hosts_file => ssh_known_hosts_file,
-                                                       :forward_agent => false))
+                                            opts.merge(:user_known_hosts_file => ssh_known_hosts_file))
       rescue Net::SSH::HostKeyUnknown => err
         fingerprint = ssh_prompt_for_fingerprint
         if fingerprint == err.fingerprint
